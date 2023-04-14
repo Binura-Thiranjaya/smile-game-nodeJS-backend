3f0562acfad8b8d65f6a961d3a1cefda8ec96892
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-04-14 19:57:45 +0530

    use hash code

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,6 @@
 import fakeUser from "../data/fakeUsers.json" assert { type: "json" };
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async () => {
   const users = await User.find();
@@ -7,31 +8,57 @@ export const getUsers = async () => {
 };
 
 export const getUser = async (username, password) => {
+  //check if user exists and return the user and password is decrypted
   const users = await User.find();
   const user = users.find(
-    (user) => user.name === username && user.password === password
+    (user) => user.name === username
   );
   if (user) {
-    return user;
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return user;
+    } else {
+      return "Password is incorrect";
+    }
   } else {
     return "No user found";
   }
 };
 
 export const addUser = async (user) => {
-    //check if user already exists
+    //check if user already exists get the password and encrypt add the user to the database
+    //Check if user already exists
     const users = await User.find();
     const userExists = users.find(
         (users) =>  user.email === users.email
     );
     if (userExists) {
         return "User already exists";
+    }else{
+
+    const newUser = new User(user);
+    const password = newUser.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    newUser.password = hashedPassword;
+    const result = await newUser.save();
+    return result;
     }
-    else{
-        const newUser = new User(user);
-        const result = await newUser.save();
-        return result;
-    }
+    
+
+
+    // const users = await User.find();
+    // const userExists = users.find(
+    //     (users) =>  user.email === users.email
+    // );
+    // if (userExists) {
+    //     return "User already exists";
+    // }
+    // else{
+    //     const newUser = new User(user);
+    //     const result = await newUser.save();
+    //     return result;
+    // }
 };
 
 export const getUserByID = async (id) => {
